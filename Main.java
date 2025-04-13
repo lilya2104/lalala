db6474f714f1f68837b4d75b48f7f591446f99fd
lilya <kjkjk@mail.com>
2025-04-13 20:43:18 +0300

    коммит 3 на локальный задание 2 dev

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Это добавлено в ветке main");
+        System.out.println("добавлено в задании 1");
+        System.out.println("добавлено перед заданием 2");
+    }
+}
